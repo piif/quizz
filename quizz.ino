0291aacbb42a84fd3db70f32810c4c2877f1ddba
piif <christian_lefebvre@laposte.net>
2021-11-11 10:23:39 +0100

    premier test : bouton et led 0 uniquement

---
+++ quizz.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#ifndef DEFAULT_BAUDRATE
+	#define DEFAULT_BAUDRATE 115200
+#endif
+
+#define BUTTON_0 2
+
+#define LED_0 13
+
+void setup(void) {
+	Serial.begin(DEFAULT_BAUDRATE);
+
+	pinMode(LED_0, OUTPUT);
+
+	pinMode(BUTTON_0, INPUT_PULLUP);
+
+	Serial.println("Ready.");
+}
+
+byte button = 1;
+byte led;
+
+void loop() {
+	byte b;
+	b = digitalRead(BUTTON_0);
+	if (b != button) {
+		button = b;
+		Serial.print("button : "); Serial.println(b);
+		led = !b;
+		digitalWrite(LED_0, led);
+	}
+}
