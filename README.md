dc297e47111a274459e617bff66e4017e5a17d76
piif <christian_lefebvre@laposte.net>
2021-11-11 22:31:21 +0100

    add simon game

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # quizz
-quizz buzzers with arduino
+Quizz buzzers for arduino
 
-## mode quizz, étapes
- - faire clignoter les 5 pendant 1 ou 2 secondes
- - attendre un appui sur le 0
- - allumer 0
- - attendre l'appui de 1, 2, 3 ou 4
- - le premier appuyé s'allume
- - retour à l'étape d'attente de l'appui sur 0
+Pour basculer d'un mode à l'autre rester appuyé sur le bouton 0 Plus d'une seconde
+
+## mode "quizz"
+ - Jingle.
+ - Attendre un appui sur le 0.
+ - Jingle.
+ - Allumer 0.
+ - Attendre l'appui de 1, 2, 3 ou 4.
+ - Le premier appuyé s'allume.
+ - Retour au début.
+
+ ## mode "simon"
+  - Jingle.
+  - Attendre un appui sur le 0.
+  - Jouer N notes (d'abord 1, puis une de plus à chaque tour).
+  - Attendre qu'on rejoue les mêmes notes.
+    À chaque pression, on attend que le button soit relaché avant de continuer (pour empécher d'en appuyer plusieurs).
+    En cas d'erreur, jouer un "buzz" à la place de la note et on recommence.
+  - Continuer jusqu'au maximum (limité à 25) et on recommence.
--- quizz.ino
+++ quizz.ino
@@ -6,6 +6,11 @@
 
 #define SPEAKER 7
 #define LONG_PRESS_DELAY 1000
+#define BUZZ_NOTE 220
+
+#define MAX_SONG 25
+#define SIMON_PLAY_DELAY 300
+#define SIMON_SILENCE_DELAY 200
 
 // Si b , Ré b , Fa , Si b
 const int notes[4] = { 466, 554, 697, 932 };
@@ -13,13 +18,15 @@ const int notes[4] = { 466, 554, 697, 932 };
 const byte buttonPins[5] = { A0, A1, A2, A3, A4 };
 const byte ledPins[5] = { 8, 9, 10, 11, 12 };
 
-byte buttons = 0b11111;
-byte leds[5]; // 0 partout par défaut
-
 enum {
+	QUIZZ_INIT,
 	QUIZZ_WAIT_START,
 	QUIZZ_WAIT_FIRST,
-	SIMON_TODO
+	SIMON_INIT,
+	SIMON_WAIT_START,
+	SIMON_PLAY,
+	SIMON_LISTEN_WAIT_PRESS,
+	SIMON_LISTEN_WAIT_RELEASE
 } step;
 
 void quizzSound() {
@@ -33,19 +40,23 @@ void quizzSound() {
 	digitalWrite(ledPins[4], 0);
 }
 
-void buzzSound() {
-	tone(SPEAKER, 220, 200);
+void quizzInit() {
+	digitalWrite(ledPins[0], 0);
+	quizzSound();
+
+	Serial.println("Waiting start");
+	step = QUIZZ_WAIT_START;
 }
 
 void quizzWaitStart() {
 	if (digitalRead(buttonPins[0]) != 0) {
 		return;
 	}
-	Serial.println("Waiting first push");
 
 	quizzSound();
 	digitalWrite(ledPins[0], 1);
 
+	Serial.println("Waiting first push");
 	step = QUIZZ_WAIT_FIRST;
 }
 
@@ -62,9 +73,161 @@ void quizzWaitFirst() {
 		}
 		m <<= 1;
 	}
-	buzzSound();
+	tone(SPEAKER, BUZZ_NOTE);
+	delay(200);
+	noTone(SPEAKER);
+
 	digitalWrite(ledPins[0], 0);
 	step = QUIZZ_WAIT_START;
+}
+
+byte songLast;
+byte song[MAX_SONG];
+byte playIndex;
+
+void simonAddNote() {
+	song[songLast] = random(0,4) + 1;
+	Serial.print("Add "); Serial.println(song[songLast]);
+	songLast++;
+}
+
+void simonSound() {
+	for(byte i = 0 ; i <= 3; i++) {
+		digitalWrite(ledPins[i], 0);
+		digitalWrite(ledPins[i+1], 1);
+		tone(SPEAKER, notes[i]);
+		delay(200);
+	}
+	noTone(SPEAKER);
+	digitalWrite(ledPins[4], 0);
+}
+
+void simonInit() {
+	digitalWrite(ledPins[0], 0);
+
+	simonSound();
+	step = SIMON_WAIT_START;
+}
+
+void simonWaitStart() {
+	if (digitalRead(buttonPins[0]) != 0) {
+		return;
+	}
+
+	randomSeed(analogRead(A5));
+	songLast=0;
+
+	simonAddNote();
+	step = SIMON_PLAY;
+}
+
+void simonPlay() {
+	digitalWrite(ledPins[0], 0);
+
+	for(byte i = 0; i < songLast; i++) {
+		digitalWrite(ledPins[song[i]], 1);
+		tone(SPEAKER, notes[song[i]-1]);
+		delay(SIMON_PLAY_DELAY);
+
+		digitalWrite(ledPins[song[i]], 0);
+		noTone(SPEAKER);
+		delay(SIMON_SILENCE_DELAY);
+	}
+
+	playIndex = 0;
+	digitalWrite(ledPins[0], 1);
+	step = SIMON_LISTEN_WAIT_PRESS;
+}
+
+// get buttons state, wait for bump, return FF if nothing changed, or new button state
+byte buttonChange() {
+	static byte buttons = 0b11110;
+
+	byte b = PINC & 0b11110;
+	if (b == buttons) {
+		return 0xFF;
+	}
+	delay(3);
+	b = PINC & 0b11110;
+	if (b == buttons) {
+		return 0xFF;
+	}
+	buttons = b;
+	Serial.print("Buttons = "); Serial.println(buttons , BIN);
+	return buttons;
+}
+
+void simonListenWaitPress() {
+	byte b = buttonChange();
+	if (b == 0xFF) {
+		return;
+	}
+
+	byte n;
+	switch(b) {
+	case 0b11100:
+		n=1;
+		break;
+	case 0b11010:
+		n=2;
+		break;
+	case 0b10110:
+		n=3;
+		break;
+	case 0b01110:
+		n=4;
+		break;
+	default:
+		n=0xFF;
+	}
+
+	Serial.print("Press "); Serial.print(n); Serial.print(", wait "); Serial.println(song[playIndex]);
+
+	if (song[playIndex] == n) {
+		digitalWrite(ledPins[n], 1);
+		tone(SPEAKER, notes[song[playIndex]-1]);
+		delay(SIMON_PLAY_DELAY);
+		digitalWrite(ledPins[n], 0);
+		noTone(SPEAKER);
+		playIndex++;
+
+		step = SIMON_LISTEN_WAIT_RELEASE;
+
+	} else {
+		if (n != 0xFF) {
+			digitalWrite(ledPins[n], 1);
+		}
+		tone(SPEAKER, BUZZ_NOTE);
+		delay(SIMON_PLAY_DELAY*2);
+		if (n != 0xFF) {
+			digitalWrite(ledPins[n], 0);
+		}
+		noTone(SPEAKER);
+
+		step = SIMON_INIT;
+	}
+}
+
+void simonListenWaitRelease() {
+	byte b = buttonChange();
+	if (b == 0xFF) {
+		return;
+	}
+	if (b == 0b11110) {
+		if (playIndex == songLast) {
+			if (playIndex == MAX_SONG) {
+				simonSound();
+				simonSound();
+				step = SIMON_INIT;
+			} else {
+				simonAddNote();
+				delay(SIMON_PLAY_DELAY*2);
+				step = SIMON_PLAY;
+			}
+		} else {
+			step = SIMON_LISTEN_WAIT_PRESS;
+		}
+	}
 }
 
 void checkLongPress() {
@@ -82,12 +245,17 @@ void checkLongPress() {
 			if (lastChange != -1 && now - lastChange > LONG_PRESS_DELAY) {
 				Serial.println("LONG PRESS RELEASED");
 				switch (step) {
+				case QUIZZ_INIT:
 				case QUIZZ_WAIT_START:
 				case QUIZZ_WAIT_FIRST:
-					step = SIMON_TODO;
+					step = SIMON_INIT;
 					break;
-				case SIMON_TODO:
-					step = QUIZZ_WAIT_START;
+				case SIMON_INIT:
+				case SIMON_WAIT_START:
+				case SIMON_PLAY:
+				case SIMON_LISTEN_WAIT_PRESS:
+				case SIMON_LISTEN_WAIT_RELEASE:
+					step = QUIZZ_INIT;
 					break;
 				}
 			}
@@ -106,8 +274,7 @@ void setup(void) {
 		pinMode(ledPins[i], OUTPUT);
 	}
 
-	quizzSound();
-	step = QUIZZ_WAIT_START;
+	step = QUIZZ_INIT; // SIMON_INIT;
 
 	Serial.println("Ready.");
 }
@@ -116,11 +283,30 @@ void loop() {
 	checkLongPress();
 
 	switch (step) {
+	case QUIZZ_INIT:
+		quizzInit();
+		break;
 	case QUIZZ_WAIT_START:
 		quizzWaitStart();
 		break;
 	case QUIZZ_WAIT_FIRST:
 		quizzWaitFirst();
+		break;
+
+	case SIMON_INIT:
+		simonInit();
+		break;
+	case SIMON_WAIT_START:
+		simonWaitStart();
+		break;
+	case SIMON_PLAY:
+		simonPlay();
+		break;
+	case SIMON_LISTEN_WAIT_PRESS:
+		simonListenWaitPress();
+		break;
+	case SIMON_LISTEN_WAIT_RELEASE:
+		simonListenWaitRelease();
 		break;
 	}
 }
